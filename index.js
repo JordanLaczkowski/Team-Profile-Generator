98108c8c25a5db5bdb56cb7f18f6fd772544cb41
Jordan <jordan.laczkowski23@gmail.com>
2022-08-30 14:44:44 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// questions = [
+//   {
+//     type: "input",
+//     message: "What is the title of your application?",
+//     name: "title",
+//   },
+//   {
+//     type: "input",
+//     message: "Describe your project.",
+//     name: "project",
+//   },
+//   {
+//     type: "list",
+//     message: "What license is your application using?",
+//     name: "license",
+//     choices: ["GNU", "Apache", "MIT"],
+//   },
+// ];
+
+//test then classes then contsruct the quesitons after everything passes
